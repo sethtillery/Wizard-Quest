84996b54db05797d09c6fe79e1f5412b0875a671
Seth Tillery <sethtillery71@gmail.com>
2022-10-13 22:00:39 -0400

    deactivates

--- Scripts/Scriptable objects/Consumable.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class Consumable : MonoBehaviour
-{
-    // Store a reference to the scriptable object asset
-    public Item item;
-}
--- Scripts/Scriptable objects/Slot.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class Slot : MonoBehaviour
-{
-    public Text qtyText;
-    public Image itemImg;
-
-}
